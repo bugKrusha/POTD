099ad17aa89dcf493172c506c694c722774f0673
Jon-Tait Beason <bugkrusha@jon-taits-mbp-2.hsd1.wa.comcast.net>
2017-04-30 15:23:41 -0700

    Initial Commit

---
+++ POTD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POTD
+//
+//  Created by Jon-Tait Beason on 4/30/17.
+//  Copyright © 2017 Jon-Tait Beason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
